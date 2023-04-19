e3377d7df88765d855fdb7a16a9aa0adf6872ff8
Ashleyrv <ashleyvianar15@gmail.com>
2023-04-19 19:37:37 -0300

    operadores

---
+++ java-basico/operadores/Operadores.java
@@ -0,0 +1,12 @@
+
+
+public class Operadores {
+    public static void main(String[] args){
+        int num = 3;
+        int num2 = 6;
+        int soma = num + num2;
+
+        System.out.println(soma);
+    }
+    
+}
---
+++ java-basico/operadores/Operadores2.java
@@ -0,0 +1,13 @@
+
+
+public class Operadores2 {
+    public static void main(String[] args){
+        int numero = 1;
+        System.out.println(numero);
+
+        //incrementação
+        System.out.println(++numero);
+
+    }
+    
+}
